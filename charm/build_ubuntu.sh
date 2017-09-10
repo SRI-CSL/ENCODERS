8d3b8f81689e3c20de557d9fbd3c8e4814ffdb05
internetofvehicles <internetofvehicles>
2017-09-10 21:19:23 +0000

    charm build ubuntu script

---
+++ charm/build_ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+sudo apt-get install python2.7 python2.7-dev libgmp-dev flex bison 
+
+
+mkdir dependencies
+pushd dependencies
+wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
+tar -xvf pbc-0.5.14.tar.gz 
+cd pbc-0.5.14/
+./configure
+make
+sudo make install
+popd
+
+./configure
+make
+sudo make install
