c931ad5ca27b235ab1ffd4dff4bb0b45c3a63dad
internetofvehicles <internetofvehicles>
2017-10-11 21:21:44 -0400

    htop

--- haggle/dependencies_ubuntu.sh
+++ haggle/dependencies_ubuntu.sh
@@ -2,5 +2,5 @@
 
 sudo timedatectl set-ntp on
 sudo apt-get update
-sudo apt-get install -y automake sqlite3 build-essential autoconf libtool git libxml2 libxml2-dev sqlite libsqlite3-dev python-dev libbluetooth-dev libdbus-1-3 libdbus-1-dev libssl-dev
+sudo apt-get install -y automake sqlite3 build-essential autoconf libtool git libxml2 libxml2-dev sqlite libsqlite3-dev python-dev libbluetooth-dev libdbus-1-3 libdbus-1-dev libssl-dev htop
 
