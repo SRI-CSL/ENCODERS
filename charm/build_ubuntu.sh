c54854e63480864f67532ecf37b90cfae15610c8
internetofvehicles <internetofvehicles>
2017-10-17 23:21:31 -0400

    remove redundancy

--- charm/build_ubuntu.sh
+++ charm/build_ubuntu.sh
@@ -7,15 +7,15 @@ mkdir dependencies
 pushd dependencies
 wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
 tar -xvf pbc-0.5.14.tar.gz 
-cd pbc-0.5.14/
+pushd pbc-0.5.14/
 ./configure
 make
 sudo make install
 popd
 
-./configure
-make
-sudo make install
+#./configure
+#make
+#sudo make install
 
 cd /usr/lib
 sudo ln -s /usr/local/lib/libpbc.so.1 .
