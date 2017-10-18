575e23a206fc5c330a736f64d08d81233a4fa59e
internetofvehicles <internetofvehicles>
2017-10-18 02:34:23 +0000

    disable time for now

--- haggle/dependencies_ubuntu.sh
+++ haggle/dependencies_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ev
 
-sudo timedatectl set-ntp on
+#sudo timedatectl set-ntp on
 sudo apt-get update
 sudo apt-get install -y automake sqlite3 build-essential autoconf libtool git libxml2 libxml2-dev sqlite libsqlite3-dev python-dev libbluetooth-dev libdbus-1-3 libdbus-1-dev libssl-dev htop
 
