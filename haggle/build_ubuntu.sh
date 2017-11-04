47b07b068718a1992a5a88493b5309749c38c64a
internetofvehicles <internetofvehicles>
2017-11-04 13:06:05 -0400

    disable mvn build for now

--- haggle/build_ubuntu.sh
+++ haggle/build_ubuntu.sh
@@ -19,8 +19,8 @@ sudo ln -s /usr/lib/jvm/java-8-openjdk-amd64/include /Headers
 make
 sudo make install
 ./dependencies.sh
-export PATH=${HOME}/installs/apache-maven-3.5.2/bin:$PATH
-mvn clean install
+#export PATH=${HOME}/installs/apache-maven-3.5.2/bin:$PATH
+#mvn clean install
 popd
 
 #arphelper
