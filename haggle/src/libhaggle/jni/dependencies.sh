893abb794334bad4271c8c366f0ec8b5b1ed0776
internetofvehicles <internetofvehicles>
2017-11-04 11:52:32 -0400

    home var

--- haggle/src/libhaggle/jni/dependencies.sh
+++ haggle/src/libhaggle/jni/dependencies.sh
@@ -7,4 +7,4 @@ pushd installs
 wget http://mirrors.gigenet.com/apache/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
 tar -xvf apache-maven-3.5.2-bin.tar.gz
 
-export PATH=/home/ubuntu/installs/apache-maven-3.5.2/bin:$PATH
+export PATH=${HOME}/installs/apache-maven-3.5.2/bin:$PATH
