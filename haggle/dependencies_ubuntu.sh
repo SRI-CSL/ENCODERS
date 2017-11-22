98bfc95fa0c4144a1aeef7956e1fcfb4c2677818
internetofvehicles <internetofvehicles>
2017-11-22 08:05:43 -0500

    add vim

--- haggle/dependencies_ubuntu.sh
+++ haggle/dependencies_ubuntu.sh
@@ -2,7 +2,7 @@
 
 #sudo timedatectl set-ntp on
 sudo apt-get update
-sudo apt-get install -y automake sqlite3 build-essential autoconf libtool git libxml2 libxml2-dev sqlite libsqlite3-dev python-dev libbluetooth-dev libdbus-1-3 libdbus-1-dev libssl-dev htop
+sudo apt-get install -y automake sqlite3 build-essential autoconf libtool git libxml2 libxml2-dev sqlite libsqlite3-dev python-dev libbluetooth-dev libdbus-1-3 libdbus-1-dev libssl-dev htop vim
 
 sudo apt-get install -y default-jdk
 
