135e30cdb8d3a187e21d683dfee70239e500b982
internetofvehicles <internetofvehicles>
2017-11-22 10:01:06 -0500

    clone if doesn't exist travel build docker

--- tools/setup_build_dockerenvironment.sh
+++ tools/setup_build_dockerenvironment.sh
@@ -3,7 +3,7 @@
 ./install_docker.sh
 
 pushd $HOME
-git clone https://github.com/vehiclecloud/ENCODERS-Docker
+test -e "ENCODERS-Docker" || git clone https://github.com/vehiclecloud/ENCODERS-Docker
 pushd ENCODERS-Docker
 sudo docker build .
 popd
